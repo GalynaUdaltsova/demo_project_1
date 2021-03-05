33b93af2ae7cfdff35321cadcd8e9fe8fe6a12a5
GalynaUdaltsova <galyna.udaltsova@gmail.com>
2021-03-05 15:21:03 +0200

    App selection was added in Test

--- src/main/java/com/softserve/task4/Test.java
+++ src/main/java/com/softserve/task4/Test.java
@@ -5,11 +5,22 @@ import com.softserve.task4.service.HumanService;
 import com.softserve.task4.service.ITesterService;
 import com.softserve.task4.service.WindowService;
 
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
 public class Test {
     public static void main(String[] args) {
         HumanService humanService = new HumanService();
-//        ITesterService tester = new ConsoleService(humanService);
-        ITesterService tester = new WindowService(humanService);
-        tester.test();
+
+        System.out.println("Choose how to open the app: window or console");
+        Scanner scanner = new Scanner(System.in);
+        String data = scanner.next();
+        if (data.equalsIgnoreCase("window")) {
+            ITesterService tester = new WindowService(humanService);
+            tester.test();
+        } else {
+            ITesterService tester = new ConsoleService(humanService);
+            tester.test();
+        }
     }
 }
